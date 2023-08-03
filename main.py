ad2147991f3573946cf545846595ae6c9de05462
SamuelCh25 <141078611+SamuelCh25@users.noreply.github.com>
2023-08-03 00:01:38 +0000

    ejercicio 10

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 #Mi Primer Programa 
+
 #Ejercicio 1
-print ( 'Hola Mundo' )
-#Ejercicio 2
-Guardar = ('Hola Mundo')
-print('Guardar')
+#Guardar = ('Hola Mundo')
+#print('Guardar')
+
+#Ejercicio 3
+#nombre=input("Escribe tu nombre: ")
+#print ('Hola,'+ nombre + ' como estas?')
+
+#Ejercicio 4 
+#print(((3+2)/(2*5))**2)
+
+#Ejercicio 5
+#numerodehoras=float(input('Escribe tu numero de horas que trabajas: '))
+#valordelahora=float(input('Escribe cuanto ganas a la hora en dinero: '))
+#pagototal=str(numerodehoras*valordelahora)
+#print('tu pago correspondiente debe ser ' + pagototal + ' pesos')
+
+#Ejercicio 6
+#enteropositivo=float(input('Introduzca un entero positivo del 1 al 20: '))
+#entero=enteropositivo
+#print('ahora realizare la siguiente operacion donde N es el entero positivo que pusiste, la formula es (N(N+1))/2')
+#resultado=str((entero*(entero+1))/2)
+#print('El Resultado Es: '+ resultado)
+
+#Ejercicio 7
+#peso=input('Introduzca su peso en Kg ')
+#estatura=input('Introduzca su estatura en M ')
+#print('Ahora calculare tu masa corporal')
+#masacorporal=round(float(peso)/float(estatura)**2,2)
+#print('tu masa corporal es ', masacorporal)
+
+#Ejercicio 8
+
+#Ejercicio 9
+#invertir=float(input( 'Cual es la cantitad a invertir? '))
+#interesanual=float(input('Cual es tu interes anual? '))
+#años=float(input('Un numero de años para su ganancia '))
+#print('Se mostrara tu capital obtenido a futuro con la formula C(i/(100+1)**años, donde C es la cantidad que vas a invertir, i tu interes anual y los años para que termine tu ganancia ')
+#capital=str((invertir)*(interesanual/((100+1)**años)))
+#print('Tu capital obtenido es: ', capital )
+
+#Ejercicio 10
